8b58c6a93049d9b212199d8f1ddcb65238ee6c1f
Hermes325 <112727212+Hermes325@users.noreply.github.com>
2023-04-16 15:31:28 +0300

    Update

--- components/slide2.js
+++ components/slide2.js
@@ -2,7 +2,7 @@ const Slide2 = () => {
     return (
         <section className="flex_center margin_top25 padding_top4">
             <div className={" au_photo mr50"}></div>
-            <div className="w28">
+            <div className="w42">
                 <h2 className="text_center">
                     О нашей компании
                 </h2>
@@ -10,7 +10,7 @@ const Slide2 = () => {
                     Ключевые слова о компании
                 </p>
                 <hr></hr>
-                <p className="text_center m_auto">
+                <p className="text_center m_auto desc">
                     Коротко представьтесь и расскажите о компании или сервисе в 3-4 строках.
                     С какими клиентами вы работаете, что вас вдохновляет. Чем гордится ваша команда,
                     какие у нее ценности и мотивация.
