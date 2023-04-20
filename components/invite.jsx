45ff276b391c6f34858562fe1c8ec771dabd3fae
Hermes325 <112727212+Hermes325@users.noreply.github.com>
2023-04-21 02:53:38 +0300

    line

--- components/invite.jsx
+++ components/invite.jsx
@@ -45,15 +45,17 @@ export default function Invite() {
     }
 
     return (<section id='invite' className={styles.invite}>
-        <h2 className={" margin_top15 flex_center text_center p_xxxlarge"}>
+        <hr className="hr2 margin_top15" />
+        <h2 className={"  margin_top2 flex_center text_center p_xxxlarge"}>
             Присоединение дома к УО
         </h2>
         <h4 className={'flex_center text_center w70 m_auto'}>
             Мы обеспечиваем поддержание и улучшение жилищных условий вашего объекта, и для сотрудничества - свяжитесь с нами
         </h4>
-        <Button colorScheme='green' onClick={onOpen} className={"button_invite flex m_auto margin_bot15"}>
+        <Button colorScheme='green' onClick={onOpen} className={"button_invite flex m_auto  "}>
             Связаться
         </Button>
+        <hr className="hr2 margin_top5 margin_bot15" />
 
         <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
